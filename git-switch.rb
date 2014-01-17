1c107563af9a28e1ebe9d4e4757a122e41f2323c
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2014-01-17 12:01:22 -0200

    Print last five branches

--- git-switch.rb
+++ git-switch.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/ruby
-
-last_branches = `git reflog | grep "checkout: moving" | cut -d' ' -f8 | head -20`.split.drop(1)
+last_branches = `git reflog | grep "checkout: moving" | cut -d' ' -f8 | head -5`.split.drop(1)
 last_branches.each_with_index do |name, index|
-puts "#{index}) #{name}"
+  puts "#{name}"
 end
-nro = $stdin.readline.to_i
-`git checkout #{last_branches[nro]}`
+#nro = $stdin.readline.to_i
+#`git checkout #{last_branches[nro]}`
