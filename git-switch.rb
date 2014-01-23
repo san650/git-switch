789d9fbaf125b55cc37c19d0f3671407e4b79ba2
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2014-01-23 12:28:51 -0200

    Remove repeated entries

--- git-switch.rb
+++ git-switch.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-last_branches = `git reflog | grep "checkout: moving" | cut -d' ' -f8 | head -5`.split.drop(1)
+last_branches = `git reflog | grep "checkout: moving" | cut -d' ' -f8 | head -8 | uniq`.split.drop(1)
 last_branches.each_with_index do |name, index|
   puts "#{name}"
 end
