637795dbba95bd2c031f3ab342b798c302191362
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2015-03-27 22:30:32 -0300

    Make expected text in tests more readable
    By removing indentation from heredoc strings

--- test/acceptance.rb
+++ test/acceptance.rb
@@ -17,60 +17,66 @@ def execute_command(*arguments)
   end
 end
 
+class String
+  def fix
+    gsub(/^\s+/, '').chomp + " "
+  end
+end
+
 describe "git-switch" do
   before do
     generate_fixture
   end
 
   it "lists branches order by modified date" do
-    execute_command("--modified").must_equal <<-EOT.chomp
-1. feature_one
-2. feature_three
-3. feature_two
-Select a branch (1-3,q) 
-EOT
+    execute_command("--modified").must_equal <<-EOT.fix
+      1. feature_one
+      2. feature_three
+      3. feature_two
+      Select a branch (1-3,q)
+    EOT
   end
 
   it "lists branches order by modified date using short argument" do
-    execute_command("-m").must_equal <<-EOT.chomp
-1. feature_one
-2. feature_three
-3. feature_two
-Select a branch (1-3,q) 
-EOT
+    execute_command("-m").must_equal <<-EOT.fix
+      1. feature_one
+      2. feature_three
+      3. feature_two
+      Select a branch (1-3,q)
+    EOT
   end
 
   it "lists branches order by checked out date" do
-    execute_command("--checked-out").must_equal <<-EOT.chomp
-1. feature_two
-2. feature_three
-3. feature_one
-Select a branch (1-3,q) 
-EOT
+    execute_command("--checked-out").must_equal <<-EOT.fix
+      1. feature_two
+      2. feature_three
+      3. feature_one
+      Select a branch (1-3,q)
+    EOT
   end
 
   it "lists branches order by checked out date using short argument" do
-    execute_command("-o").must_equal <<-EOT.chomp
-1. feature_two
-2. feature_three
-3. feature_one
-Select a branch (1-3,q) 
-EOT
+    execute_command("-o").must_equal <<-EOT.fix
+      1. feature_two
+      2. feature_three
+      3. feature_one
+      Select a branch (1-3,q)
+    EOT
   end
 
   it "limits the number of branches to show" do
-    execute_command("--count 2").must_equal <<-EOT.chomp
-1. feature_one
-2. feature_three
-Select a branch (1-2,q) 
-EOT
+    execute_command("--count 2").must_equal <<-EOT.fix
+      1. feature_one
+      2. feature_three
+      Select a branch (1-2,q)
+    EOT
   end
 
   it "limits the number of branches to show using short argument" do
-    execute_command("-c 2").must_equal <<-EOT.chomp
-1. feature_one
-2. feature_three
-Select a branch (1-2,q) 
-EOT
+    execute_command("-c 2").must_equal <<-EOT.fix
+      1. feature_one
+      2. feature_three
+      Select a branch (1-2,q)
+    EOT
   end
 end
